100b07615a4ea02373f5c42dd1bc04ee638cb136
Richalbert <richalbert@free.fr>
2024-06-17 18:35:59 +0200

    for-loop in template

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Richalbert'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': "Today it's a beautiful day"
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': "Because the sun shine"
+        }
+    ]
     # page = render_template('index.html', title='Home Page', user=user, toto='toto')
-    page = render_template('index.html', user=user, toto='toto')
+    # page = render_template('index.html', user=user, toto='toto')
+    page = render_template('index.html', user=user, posts=posts)
     return page
