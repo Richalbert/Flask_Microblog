39f0edf56bd638f0f66421023ae022460bba07a7
Richalbert <richalbert@free.fr>
2024-06-17 15:59:18 +0200

    essai d'affichage de home page index

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 # app/routes.py : Home page route
 
+
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World - Flask"
+
+    user = {'username': 'Richalbert'}
+    page = '''
+    <htm>
+        <head>
+            <title>Home Page - MicroBlog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
+    return page
