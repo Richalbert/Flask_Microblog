a06c48e5c5be576e188250b6f2d6ed6a2f89a24b
Richalbert <richalbert@free.fr>
2024-06-25 19:24:32 +0200

    restructuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 # app/__init__.py : Flask application instance
 
+# from <package> import <module> 
+
+# import a partir du package flask la classe Flask du module Flask
 from flask import Flask
 
+# on cree l'instance de la classe Flask
 app = Flask(__name__)
 
+# import de
 from app import routes
--- microblog.py
+++ microblog.py
@@ -1,3 +1,5 @@
 # microblog.py : Main application module
 
+# import de l'instance app de l'application Flask a partir du package app
 from app import app
+
